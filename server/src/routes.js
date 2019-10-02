b7cb4c83d8b75894bac736c0fd9aa686bcb2a3c1
Anderson Rodrigues <anddersonrds@gmail.com>
2019-10-01 23:42:55 -0300

    routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/uploads");
+
+const BookingController = require("./controllers/BookingController");
+const DashboardController = require("./controllers/DashboardController");
+const SessionController = require("./controllers/SessionController");
+const SpotController = require("./controllers/SpotController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
+routes.post("/sessions", SessionController.store);
+
+routes.get("/spots", SpotController.index);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+
+routes.get("/dashboard", DashboardController.show);
+
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 module.exports = routes;
