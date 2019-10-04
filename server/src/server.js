3450381ca499c6e351f2bb5ee75f723a95ac4a4e
Anderson Rodrigues <anddersonrds@gmail.com>
2019-10-03 21:44:11 -0300

    add virtualSchedule

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const routes = require("./routes");
 
 const app = express();
 
@@ -15,6 +17,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
