7397d6f412cefbe0193870746d2794a559df3785
Anderson Rodrigues <anddersonrds@gmail.com>
2019-10-14 23:56:37 -0300

    finale login's page

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
+  AsyncStorage,
   KeyboardAvoidingView,
   Image,
   Platform,
@@ -14,9 +15,17 @@ import api from "../services/api";
 
 import logo from "../assets/logo.png";
 
-export default function Login() {
+export default function Login({ navigation }) {
   const [email, setEmail] = useState("");
   const [techs, setTechs] = useState("");
+
+  useEffect(() => {
+    AsyncStorage.setItem("user").then(user => {
+      if (user) {
+        navigation.navigate("List");
+      }
+    });
+  }, []);
 
   async function handleSubmit() {
     const response = await api.post("/sessions", {
@@ -25,7 +34,10 @@ export default function Login() {
 
     const { _id } = response.data;
 
-    console.log(_id);
+    await AsyncStorage.setItem("user", _id);
+    await AsyncStorage.setItem("techs", techs);
+
+    navigation.navigate("List");
   }
 
   return (
